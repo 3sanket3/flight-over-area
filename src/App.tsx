1c5faefaf808c17b36d68b22a80c5e2b38a95e72
Sanket Patel <3sanket3@gmail.com>
2020-04-01 10:56:39 +0530

    draw a poligon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, Polygon, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { LatLngTuple } from "leaflet";
+import data from "./data.json";
 
 function App() {
+  const polygons: LatLngTuple[][] = data.features[0].geometry.coordinates as [
+    number,
+    number
+  ][][];
+
+  // const center = { lat: 51.505, lng: -0.09 };
+  const center: LatLngTuple = [-83.35447311401367, 42.23982914405];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        center={center}
+        zoom={10}
+        style={{ height: "400px", width: "400px" }}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {polygons.map(polygon => (
+          <Polygon color="purple" positions={polygon} />
+        ))}
+      </Map>
     </div>
   );
 }
