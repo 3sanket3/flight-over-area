ae7d9b5d23debffcb79a84ae3b3c573671eab6d1
Sanket Patel <3sanket3@gmail.com>
2020-04-02 16:34:14 +0530

    fix : line drawing and intersect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Map, Polygon, FeatureGroup, TileLayer } from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
-import { LatLngTuple } from "leaflet";
+import { LatLngTuple, polyline } from "leaflet";
 import data from "./data.json";
+import lineIntersect from "@turf/line-intersect";
+import { lineString, polygon } from "@turf/helpers";
 
 function App() {
+  const line = [
+    [-83.1955465039878, 43.22570800781251],
+    [-83.23060148584167, 41.32507324218751],
+    [-83.26354389726245, 43.14880371093751],
+    [-83.27448920241004, 41.05041503906251]
+  ];
 
-  const line1 = [[-83.28605905489202,42.77526855468751],[-83.2616105331007,42.6544189453125]]
-  const line2 = [[-83.3251208571526,42.73681640625001],[-83.36205174464672,41.385498046875]]
   const polygons: LatLngTuple[][] = data.features[0].geometry.coordinates as [
     number,
     number
   ][][];
 
-  // const center = { lat: 51.505, lng: -0.09 };
-  const center: LatLngTuple = [-83.35447311401367, 42.23982914405];
+  const [lines, setLines] = useState<[number, number][]>([]);
+  const [isIntersect, setIsIntersect] = useState(false);
+  const center: LatLngTuple = polygons[0][0];
 
+  function onLineCreate(e: any) {
+    const latlngs = e.layer.editing.latlngs;
+    const convertedLatLngs = latlngs.map((ltn: any) =>
+      ltn.map((l: any) => [l.lat, l.lng])
+    );
+
+    setLines(convertedLatLngs[0]);
+  }
+
+  useEffect(() => {
+    if (lines.length && polygons.length) {
+      console.log({ lines, polygons });
+
+      const intersectRes = lineIntersect(lineString(lines), polygon(polygons));
+      if (intersectRes.features.length) {
+        setIsIntersect(true);
+      } else {
+        setIsIntersect(false);
+      }
+    } else {
+      setIsIntersect(false);
+    }
+  }, [lines, polygons]);
   return (
     <div className="App">
-      <Map center={center} zoom={8} style={{ height: "100vh", width: "100vw" }}>
+      <Map center={center} zoom={8} style={{ height: "90vh", width: "100vw" }}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -28,8 +58,9 @@ function App() {
           <EditControl
             position="topright"
             // onEdited={(e: any) => console.log(e)}
-            onCreated={(e: any) => console.log(e)}
-            // onDeleted={(e: any) => console.log(e)}
+            onCreated={(e: any) => onLineCreate(e)}
+            onDeleted={(e: any) => setLines([])}
+            onCancel={(e: any) => setLines([])}
             draw={{
               rectangle: false
             }}
@@ -39,6 +70,12 @@ function App() {
           <Polygon color="purple" positions={polygon} />
         ))}
       </Map>
+
+      {isIntersect ? (
+        <span style={{ color: "red" }}>Danger</span>
+      ) : (
+        <span style={{ color: "green" }}>safe</span>
+      )}
     </div>
   );
 }
